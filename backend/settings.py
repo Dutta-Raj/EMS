a2128df502a42a6c359e16f65f8272f5a0159054
Rajdeep Dutta <155871987+Dutta-Raj@users.noreply.github.com>
2025-10-31 23:45:22 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -53,11 +53,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'employee_management',
+        'USER': 'user1',
+        'PASSWORD': 'user1',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
